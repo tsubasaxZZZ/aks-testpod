92000aee7f52eeef5f6e94b39d2dd35411c0aacd
Tsubasa Nomura <tsubasa@nomupro.com>
2020-06-04 00:15:23 +0900

    Big update for signal.go

--- signal.go
+++ signal.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -20,31 +21,48 @@ func main() {
 	flag.Parse()
 
 	h, _ := os.Hostname()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		a := r.RemoteAddr
 		q := r.URL.RequestURI()
 		log.Printf("Connection start: %s, URI=%s", a, q)
-		fmt.Fprintf(w, "Connection suceed: duration=%d, hostname=%s, requestURI=%s\n", *d, h, q)
+		fmt.Fprintf(w, "Connection start: duration=%d, hostname=%s, requestURI=%s\n", *d, h, q)
+		w.(http.Flusher).Flush()
 		for i := *d; i > 0; i-- {
 			log.Printf("something to do(%ds)...: %s, URI=%s\n", i, a, q)
 			time.Sleep(time.Second * 1)
 		}
 		log.Printf("Connection end: %s, URI=%s\n", a, q)
-	})
+		fmt.Fprintf(w, "Connection end: duration=%d, hostname=%s, requestURI=%s\n", *d, h, q)
+		w.(http.Flusher).Flush()
+	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	server := &http.Server{Addr: ":80", Handler: mux}
 
+	idleConnsClosed := make(chan struct{})
 	go func() {
-		http.ListenAndServe(":80", nil)
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGTERM, syscall.SIGQUIT, os.Interrupt)
+		sig := <-sigs
+		log.Printf("Terminate start: signal=%s\n", sig)
+		for i := *t; i > 0; i-- {
+			log.Printf("Terminate processing(%ds)...", i)
+			time.Sleep(time.Second * 1)
+		}
+
+		log.Printf("Terminate end: signal=%s\n", sig)
+
+		// We received an interrupt signal, shut down.
+		if err := server.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
 	}()
 
-	sigs := make(chan os.Signal)
-	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
-	sig := <-sigs
-
-	log.Printf("Terminate start: signal=%s\n", sig)
-	for i := *t; i > 0; i-- {
-		log.Printf("Terminate processing(%ds)...", i)
-		time.Sleep(time.Second * 1)
+	if err := server.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatal(err)
 	}
-	log.Printf("Terminate end: signal=%s\n", sig)
+	<-idleConnsClosed
 
 }
